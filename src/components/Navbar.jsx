4097c8623d9b592d31d4d6f52099b5d8bdbac1f4
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-09-08 15:51:55 +0800

    Finish navbar Icon & Icon Title Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar(){
 
   return(<>
     <div className="min-h-[70px] border-b-[1px] border-[#414141] flex border-solid px-[240px] items-center">
-      <div className='ml-[114px] flex gap-4 items-center '>
+      <div className='ml-[114px] mr-[127px] flex gap-4 items-center '>
       <img src={navbarIcon} alt="navBarIcon" className='' />
-        <p className='font-Roboto'>Wheel Care</p>
+        <p className='font-Roboto font-bold text-2xl'>Wheel Care</p>
       </div>
     </div>
 
