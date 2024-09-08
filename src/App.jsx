ab50b7134328102411b5a6100c65849c8903c697
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-09-08 11:47:31 +0800

    Initiate all dependencies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='min-h-lvh bg-slate-800'>
+    </div>
+    </>
+  )
+}
+
+export default App
