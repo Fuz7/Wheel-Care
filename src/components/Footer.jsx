672cd25fc217f7cae4b9b785a5a130aedfc1c3cb
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-10-16 18:56:44 +0800

    Add responsiveness on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,19 @@ export default function Footer() {
       className="mt-auto min-w-full flex justify-center
      min-h-[250px] bg-[#272932]"
     >
-      <div className="max-w-[1440px] min-w-[1440px] pt-[16px] flex">
+      <div className="max-w-[1440px] min-w-full items-center  pt-[16px] pb-[60px] lg:pb-0 flex 
+      flex-col lg:flex-row lg:justify-center">
         <div className="flex items-center gap-[8px]">
           <img src={footerLogo} alt="Footer Logo" />
           <p
-            className="max-w-[297px] min-w-[297px] text-[36px]
+            className="max-w-[297px] min-w-[297px [font-size:clamp(20px,5vw,32px)] lg:text-[36px]
           font-Inter font-medium text-white mb-[4px]"
           >
             Rolling Towards Independence, Built for You.
-          </p>
+          </p>  
         </div>
-        <div className="mt-[48px] ml-[286px] flex flex-col gap-[21px]">
+        <div className="mt-[48px]  lg:self-start
+        lg:ml-[100px] xl:ml-[170px] 2xl:ml-[286px] flex flex-col gap-[21px]">
           <div
             className="text-[20px] text-white 
           cursor-pointer font-Poppins-SemiBold"
@@ -36,7 +38,8 @@ export default function Footer() {
             Testimonials
           </div>
         </div>
-        <div className="mt-[72px] ml-[250px] flex flex-col gap-4">
+        <div className="mt-[72px] lg:self-start
+        lg:ml-[100px]  xl:ml-[150px] 2xl:ml-[250px] flex flex-col gap-4">
           <div className="text-[20px] font-Poppins-SemiBold text-white">
             Sign up to our Newsletter
           </div>
